8b1fc019c074c4a2dd14969e55423e8aa101635e
yoyomanyoyo87 <yoyomanyoyo87@gmail.com>
2019-06-30 18:22:20 -0400

    Mewtwo

--- src/App.js
+++ src/App.js
@@ -24,13 +24,17 @@ class App extends Component{
   }
   componentDidMount = async () => {
     const { userSession } = getConfig()
+
+    if(userSession.isUserSignedIn()){
+      console.log('User is Signed In')
+    }
     
     console.log("App.js Component Did Mount")
     if (!userSession.isUserSignedIn() && userSession.isSignInPending()) {
       console.log("Pending Sign in")
       const userData = await userSession.handlePendingSignIn()
       console.log("Finished Signin")
-      await User.createWithCurrentUser()
+      // await User.createWithCurrentUser()
       console.log("User Created with Radiks")
       if (!userData.username) {
         throw new Error('This app requires a username')
