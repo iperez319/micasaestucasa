d822d3ac938816eae4192fcfd46589862af00968
yoyomanyoyo87 <yoyomanyoyo87@gmail.com>
2019-06-30 17:58:11 -0400

    More debugging

--- src/App.js
+++ src/App.js
@@ -23,9 +23,11 @@ class App extends Component{
   componentDidMount = async () => {
     const { userSession } = getConfig()
     
-
+    console.log("App.js Component Did Mount")
     if (!userSession.isUserSignedIn() && userSession.isSignInPending()) {
+      console.log("Pending Sign in")
       const userData = await userSession.handlePendingSignIn()
+      console.log("Finished Signin")
       await User.createWithCurrentUser()
       console.log("User Created with Radiks")
       if (!userData.username) {
